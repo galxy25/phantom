4685ed83f2dfa67b520bd955b609090d51fbed87
Levi Schoen <levismschoen@gmail.com>
2019-04-29 20:58:35 -0700

    Do busy work and set longer timeouts to prevent premature termination. - Update crontab runner to have a path that allows it to run selenium-server.

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/galxy25/phantom
 go 1.12
 
 require (
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/sclevine/agouti v3.0.0+incompatible
+	golang.org/x/net v0.0.0-20190424112056-4829fb13d2c6 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	golang.org/x/sys v0.0.0-20190428183149-804c0c7841b5 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- phantom.go
+++ phantom.go
@@ -1,3 +1,17 @@
+/*
+Levis-MacBook-Pro:phantom levischoen$ which selenium-server
+/usr/local/bin/selenium-server
+$ go install
+$ crontab -e
+    # https://stackoverflow.com/questions/10129381/crontab-path-and-user
+    SHELL=/bin/sh
+    PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/levischoen/go/bin/:/Users/levischoen/.rvm/bin
+    # Launch phantom program to play sunday night slow jams
+    # Start five minutes before the show begins to ensure the
+    # audio is playing by the time the consuming program is initialized
+    # https://crontab.guru/#55_21_*_*_SUN
+    55 21 * * SUN phantom
+*/
 package main
 
 import (
@@ -6,22 +20,38 @@ import (
     "time"
 )
 
-/*
-$ go install
-$ crontab -e
-    # Launch phantom program to play sunday night slow jams
-    # Start five minutes before the show begins to ensure the
-    # audio is playing by the time the consuming program is initialized
-    # https://crontab.guru/#55_21_*_*_SUN
-    55 21 * * SUN phantom
-*/
+const (
+    headStartMinutes   = 5
+    slowJamzSeconds    = 144000 // 1000 * 60 * 60 * 4  = 4 hours
+    logIntervalSeconds = 30
+)
+
+func TailLogs(page *agouti.Page) {
+    for {
+        logTypes, err := page.LogTypes()
+        if err != nil {
+            log.Printf("error %s retrieving page %+v logtypes\n", err, page)
+        }
+        log.Printf("available log types: %v\n", logTypes)
+        for _, logType := range logTypes {
+            logs, err := page.ReadNewLogs(logType)
+            if err != nil {
+                log.Printf("error %s retrieving page %+v %s logs\n", err, page, logType)
+            }
+            log.Printf("%s logs: %v\n", logType, logs)
+        }
+        time.Sleep(logIntervalSeconds * time.Second)
+    }
+}
+
 func main() {
+    timeout := agouti.Timeout(slowJamzSeconds)
     // https://agouti.org
-    driver := agouti.Selenium()
+    driver := agouti.Selenium(timeout)
     if err := driver.Start(); err != nil {
         log.Printf("Failed to start Selenium: %s", err)
     }
-    page, err := driver.NewPage(agouti.Browser("chrome"))
+    page, err := driver.NewPage(agouti.Browser("chrome"), timeout)
     if err != nil {
         log.Printf("Failed to open page: %s", err)
     }
@@ -44,8 +74,10 @@ func main() {
         `, arguments, &result)
     time.Sleep(5 * time.Second)
     page.RunScript("$(\".css-wwzdid\").click();", arguments, &result)
+    // Do some busy work to ensure the session doesn't prematurely timeout
+    go TailLogs(page)
     // Let the slow jamz begin
-    time.Sleep(4*time.Hour + 5*time.Minute)
+    time.Sleep(slowJamzSeconds*time.Second + headStartMinutes*time.Minute)
     if err := driver.Stop(); err != nil {
         log.Printf("Failed to close pages and stop WebDriver: %s", err)
     }
